ecf9ca569c89ef6f12f88e8a2c13933b743ca805
Kris Kristofferson Nabua <kc.nabua@goabroad.com>
2020-02-07 14:01:45 +0800

    Workshop Start

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -23,7 +23,7 @@ $app = new Laravel\Lumen\Application(
 
 // $app->withFacades();
 
-// $app->withEloquent();
+$app->withEloquent();
 
 /*
 |--------------------------------------------------------------------------
@@ -76,6 +76,7 @@ $app->singleton(
 |
 */
 
+$app->register(Flipbox\LumenGenerator\LumenGeneratorServiceProvider::class);
 // $app->register(App\Providers\AppServiceProvider::class);
 // $app->register(App\Providers\AuthServiceProvider::class);
 // $app->register(App\Providers\EventServiceProvider::class);
--- routes/web.php
+++ routes/web.php
@@ -10,24 +10,23 @@
 | and give it the Closure to call when that URI is requested.
 |
 */
-/*
-|$router->get('/', function () use ($router) {
-|    return true;
-|});
- */
 
-/*
- * 🦊 Create a GET Endpoint
- * /
+//🦊 Create the todo model
+//🦊 Create the todo controller
 
-/*
- * 🦊 Create a POST Endpoint
- * /
+$router->get('/', function () use ($router) {
+    return $router->app->version();
+});
 
-/*
- * 🦊 Create a PUT Endpoint
- * /
+$router->group(['prefix' => '/todos'], function ($router) {
+//🦊 Create a get endpoint
 
-/*
- * 🦊 Create a DELETE Endpoint
- * /
+//🦊 Create a post endpoint
+
+//🦊 Create a put endpoint
+
+//🦊 Create a delete endpoint
+});
+
+// Next Workshop Authorization
+// Next Workshop Migration/Seeder
